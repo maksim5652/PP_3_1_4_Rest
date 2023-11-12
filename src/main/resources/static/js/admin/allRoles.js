fb2b4c04013448e7bc3ae5fa06673a4c33c84441
maksim5652 <86925671+maksim5652@users.noreply.github.com>
2023-11-12 21:35:41 +0300

    Delete src directory

--- src/main/resources/static/js/admin/allRoles.js
+++
@@ -1,7 +0,0 @@
-const getRolesUrl = '/api/roles'
-
-function getRolesRequest() {
-    return fetch(getRolesUrl)
-        .then(response => response.json())
-        .catch(err => console.error(err))
-}
